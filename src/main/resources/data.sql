8792d05a19b83c92018cd4521abc4a9ff1e954ef
Anton Saushkin <saushkin_anton@mail.ru>
2020-07-12 23:35:44 +0300

    Add auto-initialization-dml

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO People (name, age) VALUES ('Ivan', 20);
+INSERT INTO People (name, age) VALUES ('Anton', 23);
+INSERT INTO People (name, age) VALUES ('Peter', 25);
+INSERT INTO People (name, age) VALUES ('German', 29);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS People;
+CREATE TABLE People(
+    id SERIAL,
+    name VARCHAR(255),
+    age int
+);
