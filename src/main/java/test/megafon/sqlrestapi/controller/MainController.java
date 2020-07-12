6ac7dfcb0149f2b6c0fedb9831ed21fdb8af25eb
Anton Saushkin <saushkin_anton@mail.ru>
2020-07-12 17:08:09 +0300

    Final

--- src/main/java/test/megafon/sqlrestapi/controller/MainController.java
+++ src/main/java/test/megafon/sqlrestapi/controller/MainController.java
@@ -1,7 +1,6 @@
 package test.megafon.sqlrestapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import test.megafon.sqlrestapi.model.People;
 import test.megafon.sqlrestapi.service.PeopleService;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 public class MainController {
@@ -19,16 +17,8 @@ public class MainController {
     public MainController(PeopleService pService){
         this.pService = pService;
     }
-    @RequestMapping(value = "/people", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<People> getAll(){
         return pService.getAllPeople();
-    }
-    @RequestMapping(value = "/people/{id}", method = RequestMethod.GET)
-    public People getOne(@PathVariable int id){
-        try{
-            return pService.getOnePeople(id);
-        } catch (NoSuchElementException e) {
-            return null;
-        }
     }
 }
